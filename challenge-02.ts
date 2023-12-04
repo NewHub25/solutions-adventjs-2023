06cd1d35401f775834b4e5b9f694bd04f3c13c2b
Terry <103373722+NewHub25@users.noreply.github.com>
2023-12-04 11:27:24 -0500

    Create challenge-02.ts

---
+++ challenge-02.ts
@@ -0,0 +1,19 @@
+function manufacture(gifts: string[], materials: string) {
+  const result = [];
+  for (const gift of gifts) {
+    let temp = '';
+    for (const cc of gift) {
+      if (materials.indexOf(cc) < 0) break;
+      temp += cc;
+    }
+    temp === gift && result.push(gift);
+  }
+  return result;
+}
+
+/*
+Let's analyze the code in terms of its time complexity (Big O notation):
+O(m*n)
+The outer loop iterates through each gift in the gifts array. Let's denote the number of gifts as n.
+The inner loop iterates through each character in a specific gift. Let's denote the maximum length of a gift as m.
+*/
